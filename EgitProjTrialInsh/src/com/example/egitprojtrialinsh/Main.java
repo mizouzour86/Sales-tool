36c52459dad726a6ad808f10567fcf4e0ab0da9b
Mohammed <Mohammed@Mohammed-PC.home>
2014-03-13 19:41:43 +0000

    add a simple change

---
+++ EgitProjTrialInsh/src/com/example/egitprojtrialinsh/Main.java
@@ -0,0 +1,17 @@
+package com.example.egitprojtrialinsh;
+
+import com.example.egitprojtrialinsh.SalesData;
+
+public class Main {
+	public static void main(String[] args) {
+		SalesData data= new SalesData();
+		displayGreeting();
+		data.display();
+	} 
+
+	private static void displayGreeting() {
+		System.out.println("Hello happy sales people");
+		System.out.println("This app shows sales data");
+System.out.println("first change");
+	}
+}
