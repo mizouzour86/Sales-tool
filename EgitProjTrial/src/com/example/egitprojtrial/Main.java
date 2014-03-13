f5b7b408a0ce1b525805227e974cc611e968a80e
Mohammed <Mohammed@Mohammed-PC.home>
2014-03-13 17:48:37 +0000

    Initial checking of sales tool

---
+++ EgitProjTrial/src/com/example/egitprojtrial/Main.java
@@ -0,0 +1,11 @@
+package com.example.egitprojtrial;
+public class Main {
+public static void main (String[] args){
+	displayGreeting();
+	data.display();
+}
+private static void displayGreeting(){
+	System.out.println("Hello happy sales people");
+	System.out.println("This app shows sales data");
+}
+}
---
+++ EgitProjTrial/src/com/example/egitprojtrial/SalesData.java
@@ -0,0 +1,8 @@
+package com.example.egitprojtrial;
+
+public class SalesData {
+int data[] ={0,4,42};
+public void display(){
+	System.out.println("TODO: implement data diplay");
+}
+}
